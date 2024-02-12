643886c2277176481dcea592daa7a85d084176a5
Nassar <nassarragi@gmail.com>
2024-02-12 00:18:46 +0000

    Dictionary to speech

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,7 @@
 import click
 import json
 import requests
+from playsound import playsound
 
 @click.command()
 @click.option('--word', prompt='Enter a word')
@@ -12,6 +13,12 @@ def dictionary(word):
         data = response.json()
         print(f"Word: {word}")
         print(f"Meaning: {data[0]['meanings'][0]['definitions'][0]['definition']}")
+        audio = data[0]['phonetics'][0]['audio']
+        audio_file = f"{word}.mp3"
+        response = requests.get(audio)
+        with open(audio_file, "wb") as file:
+            file.write(response.content)
+        playsound(audio_file)
         
     elif response.status_code != 200:
         print("Sorry, there was an error. Please try again.")
